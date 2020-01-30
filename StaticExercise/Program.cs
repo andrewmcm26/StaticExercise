ec9d26d865d0b4dea6f75261d511def2955142c7
Andrew McMahon <andrewmcm26@gmail.com>
2020-01-30 09:32:38 -0600

    Initial commit - Celsius to Farenheit (and vice versa) converter

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var temp1 = TempConverter.CelsiusToFarenheit(0);
+            Console.WriteLine(temp1);
+
+            var temp2 = TempConverter.FarenheitToCelcius(32);
+            Console.WriteLine(temp2);
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+       
+
+        public static double FarenheitToCelcius(double f)
+        {
+            return (f - 32) * (5 / 9);
+        }
+
+        public static double CelsiusToFarenheit(double c)
+        {
+            return (c * (9 / 5)) + 32;
+        }
+    }
+}
